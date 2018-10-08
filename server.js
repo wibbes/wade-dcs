c9b8baf689b86bf9146c3168e0cbde5e35d7944d
james lyons <lobster94@outlook.com>
2018-10-08 22:30:17 +0100

    Update server.js

--- server.js
+++ server.js
@@ -12,7 +12,7 @@ const server = express()
   .listen(PORT, () => console.log(`Listening on ${ PORT }`));
 
 const io = socketIO(server);
-
+ console.log('Server Started');
 io.on('connection', (socket) => {
   console.log('Client connected');
   socket.on('disconnect', () => console.log('Client disconnected'));
