02159a33328ac6dc6a9c9c9731fea456de1f7ff0
james lyons <lobster94@outlook.com>
2018-10-08 22:21:25 +0100

    1

--- server.js
+++ server.js
@@ -16,6 +16,10 @@ const io = socketIO(server);
 io.on('connection', (socket) => {
   console.log('Client connected');
   socket.on('disconnect', () => console.log('Client disconnected'));
+  socket.on('Head1', function (data) {
+    console.log("Head 1 -" + data);
+    socket.emit('Head1', { my: 'data' });
+  });
 });
 
 setInterval(() => io.emit('time', new Date().toTimeString()), 1000);
