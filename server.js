87d8731ff4e43e901e8c9bd2e6a0b50994cc31c5
james lyons <lobster94@outlook.com>
2018-10-09 20:53:54 +0100

    Update server.js

--- server.js
+++ server.js
@@ -16,10 +16,10 @@ const io = socketIO(server);
 io.on('connection', (socket) => {
   console.log('Client connected');
   socket.on('disconnect', () => console.log('Client disconnected'));
-	socket.on('Head1', function(data){ 
-   console.log('Client disconnected')
-   socket.emit('Head1',data);
-   );
+  socket.on('Head1', function(data){ 
+	console.log('Client disconnected')
+	socket.emit('Head1',data);
+  });
 });
 
 setInterval(() => io.emit('time', new Date().toTimeString()), 1000);
