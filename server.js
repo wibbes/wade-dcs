a2bcf7af7ea574db5335aa745b487fa1e93b900d
james lyons <lobster94@outlook.com>
2018-10-11 03:42:32 +0100

    Update server.js

--- server.js
+++ server.js
@@ -16,7 +16,7 @@ const server = express()
 const io = socketIO(server);
 
 io.on('connection', (socket) => {
-	console.log(conn.handshake.headers.referer);
+	console.log(socket.handshake.headers.referer);
 	
   console.log('Client connected');
   socket.on('disconnect', () => console.log('Client disconnected'));
