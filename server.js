26fad490268027f5bda9157111179e7a368e06c1
james lyons <lobster94@outlook.com>
2018-10-11 03:45:48 +0100

    Update server.js

--- server.js
+++ server.js
@@ -16,9 +16,13 @@ const server = express()
 const io = socketIO(server);
 
 io.on('connection', (socket) => {
-	console.log(socket.handshake.headers.referer);
-	
+console.log(socket.handshake.headers.referer);
+	const myURL = new URL(socket.handshake.headers.referer);
+console.log(myURL.searchParams.get('machine'));
+console.log(myURL.searchParams.get('payload'));
+  
   console.log('Client connected');
+  
   socket.on('disconnect', () => console.log('Client disconnected'));
   socket.on('Machine', function(data){ 
 	console.log('Client disconnected')
