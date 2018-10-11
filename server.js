a05dfbdc624da3c7e12c17fcbcded958be59a2b0
james lyons <lobster94@outlook.com>
2018-10-11 03:41:06 +0100

    Update server.js

--- server.js
+++ server.js
@@ -8,19 +8,16 @@ const PORT = process.env.PORT || 3000;
 const INDEX = path.join(__dirname, 'index.html');
 
 const server = express()
+  .use((req, res) =>{ res.sendFile(INDEX) })
   .listen(PORT, () => console.log(`Listening on ${ PORT }`));
 
-server.get('/api/users', function(req, res) {
-  var user_id = req.param('id');
-  var token = req.param('token');
-  var geo = req.param('geo');  
 
-  res.send(user_id + ' ' + token + ' ' + geo);
-});
   
 const io = socketIO(server);
 
 io.on('connection', (socket) => {
+	console.log(conn.handshake.headers.referer);
+	
   console.log('Client connected');
   socket.on('disconnect', () => console.log('Client disconnected'));
   socket.on('Machine', function(data){ 
