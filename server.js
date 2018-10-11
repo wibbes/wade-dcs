725b79565f137bfbfbefbef37766aaf1c3aa3ca7
james lyons <lobster94@outlook.com>
2018-10-11 03:31:30 +0100

    Update server.js

--- server.js
+++ server.js
@@ -8,11 +8,11 @@ const PORT = process.env.PORT || 3000;
 const INDEX = path.join(__dirname, 'index.html');
 
 const server = express()
-  .use((req, res) =>{
-	  res.sendFile(INDEX) ;
+  server.use((req, res) =>{
+	  res.sendFile(INDEX);
 	  console.log(req);
 	  });
-  .listen(PORT, () => console.log(`Listening on ${ PORT }`));
+  server.listen(PORT, () => console.log(`Listening on ${ PORT }`));
 
 const io = socketIO(server);
 
