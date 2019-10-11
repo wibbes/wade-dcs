c208dc4e5d7d22e6b236d2dca4534c38fe43003f
wibbes <jamwillyo@gmail.com>
2019-10-11 20:53:38 +0100

    Update app.js

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const express = require('express')
 const path = require('path')
 const PORT = process.env.PORT || 5000
+var nodeOutlook = require('nodejs-nodemailer-outlook')
 
 express()
   .use(express.static(path.join(__dirname, 'public')))
@@ -28,7 +29,7 @@ express()
     replyTo: 'james.lyons@wade.co.uk',
     onError: (e) => console.log(e),
     onSuccess: (i) => console.log(i)
-}
+    });  
   
   });
         
