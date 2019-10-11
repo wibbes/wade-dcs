879ef4529070b2b4e619d3f0598257fd58658568
wibbes <jamwillyo@gmail.com>
2019-10-11 20:50:45 +0100

    Update app.js

--- app.js
+++ app.js
@@ -7,4 +7,29 @@ express()
   .set('views', path.join(__dirname, 'views'))
   .set('view engine', 'ejs')
   .get('/', (req, res) => res.send('pages/index'))
+  .post('/email', function (req, res) {
+  
+  nodeOutlook.sendEmail({
+    host: "smtp-mail.outlook.com", // hostname
+    secureConnection: false, // TLS requires secureConnection to be false
+    port: 587, // port for secure SMTP
+    auth: {
+      user: "james.lyons@wade.co.uk",
+      pass: "Sleepy1@"
+    },
+    tls: {
+        ciphers:'SSLv3'
+    },
+    from: 'james.lyons@wade.co.uk',
+    to: 'james.lyons@wade.co.uk',
+    subject: 'Hey you, awesome!',
+    html: '<b>This is bold text</b>',
+    text: 'This is text version!',
+    replyTo: 'james.lyons@wade.co.uk',
+    onError: (e) => console.log(e),
+    onSuccess: (i) => console.log(i)
+}
+  
+  });
+        
   .listen(PORT, () => console.log(`Listening on ${ PORT }`))
