40072afe4a2701745e2e7bcf08c436eb1dbf37d6
wibbes <jamwillyo@gmail.com>
2019-10-11 20:58:23 +0100

    Update app.js

--- app.js
+++ app.js
@@ -8,29 +8,28 @@ express()
   .set('views', path.join(__dirname, 'views'))
   .set('view engine', 'ejs')
   .get('/', (req, res) => res.send('pages/index'))
+  .listen(PORT, () => console.log(`Listening on ${ PORT }`))
   .post('/email', function (req, res) {
-  
-  nodeOutlook.sendEmail({
-    host: "smtp-mail.outlook.com", // hostname
-    secureConnection: false, // TLS requires secureConnection to be false
-    port: 587, // port for secure SMTP
-    auth: {
-      user: "james.lyons@wade.co.uk",
-      pass: "Sleepy1@"
-    },
-    tls: {
-        ciphers:'SSLv3'
-    },
-    from: 'james.lyons@wade.co.uk',
-    to: 'james.lyons@wade.co.uk',
-    subject: 'Hey you, awesome!',
-    html: '<b>This is bold text</b>',
-    text: 'This is text version!',
-    replyTo: 'james.lyons@wade.co.uk',
-    onError: (e) => console.log(e),
-    onSuccess: (i) => console.log(i)
-    });  
-  
-  });
-        
-  express.listen(PORT, () => console.log(`Listening on ${ PORT }`))
+
+    nodeOutlook.sendEmail({
+      host: "smtp-mail.outlook.com", // hostname
+      secureConnection: false, // TLS requires secureConnection to be false
+      port: 587, // port for secure SMTP
+      auth: {
+        user: "james.lyons@wade.co.uk",
+        pass: "Sleepy1@"
+      },
+      tls: {
+          ciphers:'SSLv3'
+      },
+      from: 'james.lyons@wade.co.uk',
+      to: 'james.lyons@wade.co.uk',
+      subject: 'Hey you, awesome!',
+      html: '<b>This is bold text</b>',
+      text: 'This is text version!',
+      replyTo: 'james.lyons@wade.co.uk',
+      onError: (e) => console.log(e),
+      onSuccess: (i) => console.log(i)
+      });  
+
+    });
